c7d022778b38f4868049ddfec2c4b57e681e1f4a
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2023-09-26 19:21:50 +0700

    pull

---
+++ client/src/app/pages/layout/components/sidebar/sidebar.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
+
+interface Page {
+  id: number;
+  name: string;
+  link: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+  pageSelected: number = 0;
+  url = '';
+
+  pages: Page[] = [
+    {
+      id: 0,
+      name: 'Home',
+      link: 'base/home',
+      icon: 'fa-solid fa-house',
+    },
+    {
+      id: 1,
+      name: 'History',
+      link: 'base/history',
+      icon: 'fa-solid fa-clock-rotate-left',
+    },
+  ];
+  route$ = this.router.events;
+
+  constructor(private router: Router) {
+    combineLatest({
+      route: this.route$,
+    }).subscribe((res) => {
+      if (this.router.url != this.url) {
+        this.url = this.router.url;
+        this.router.url === '/base/home' ? (this.pageSelected = 0) : null;
+        this.router.url === '/base/history' ? (this.pageSelected = 1) : null;
+        // this.router.url === '/base/profile' ? (this.pageSelected = 2) : null;
+        // this.router.url === '/base/cart' ? (this.pageSelected = 3) : null;
+        // this.router.url === '/base/settings' ? (this.pageSelected = 4) : null;
+      }
+    });
+  }
+
+  selected(index: number) {
+    this.router.navigate([this.pages[index].link]);
+  }
+}
