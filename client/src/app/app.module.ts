61ed38b17b75a7ff5e3dff771f0c35690e7af4fd
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2023-09-29 13:53:28 +0700

    fix

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { carReducer } from './ngrx/reducers/car.reducer';
+import { CarEffects } from './ngrx/effects/car.effects';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      car: carReducer,
+    }),
+    EffectsModule.forRoot([CarEffects]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
