f21a29235b38af6d35537078db82f04250b8060e
PhKhoa <khoa.pm14576@sinhvien.hoasen.edu.vn>
2023-10-06 14:13:50 +0700

    pull

--- client/src/app/models/user.model.ts
+++ client/src/app/models/user.model.ts
@@ -1,11 +1,10 @@
-export interface User{
-    uid: string;
-    _id: string;
-    name: string;
-    email: string;
-    phone: string;
-    avatar: string;
-    Address: string;
-    role: string;
-
+export interface User {
+  uid: string;
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+  avatar: string;
+  Address: string;
+  role: string;
 }
--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import * as AuthAcitons from 'src/app/ngrx/actions/auth.actions';
+import { Store } from '@ngrx/store';
+import { AuthState } from 'src/app/ngrx/states/auth.state';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(
+    private store: Store<{ auth: AuthState }>,
+    private router: Router
+  ) {}
 
+  loginWithGoogle() {
+    let loginResult = this.store.dispatch(AuthAcitons.login());
+    if (loginResult == null) {
+      console.log('Login failed');
+    } else {
+      console.log('Login success');
+      this.router.navigate(['/home']);
+    }
+  }
 }
